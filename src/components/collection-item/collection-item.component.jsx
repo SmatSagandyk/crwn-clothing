5d702aa08e3c637fd8855af3cc05548a995b5d3b
Smat <sagandyksmat@gmail.com>
2021-04-06 17:55:43 +0600

    added add item and remove item to checkout page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import { connect } from 'react-redux';
-import { addItem } from "../../redux/cart/cart.actions";
+import { addItem, toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({item, addItem}) => {
+const CollectionItem = ({item, addItem, toggleCartHidden}) => {
     const {name, price, imageUrl} = item;
     return(
         <div className="collection-item">
@@ -24,7 +24,8 @@ const CollectionItem = ({item, addItem}) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    addItem: item => dispatch(addItem(item))
+    addItem: item => dispatch(addItem(item)),
+    toggleCartHidden:  () => dispatch(toggleCartHidden())
 })
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
